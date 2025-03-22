addea646d0709c57cf0323ac0aec3c50005bd27b
korikmat <korikmat@fel.cvut.cz>
2025-03-22 12:18:01 +0100

    Add route constants for genre selection and user profile editing

--- app/src/main/java/com/korikmat/watchtogether/utils/Constants.kt
+++ app/src/main/java/com/korikmat/watchtogether/utils/Constants.kt
@@ -1,7 +1,10 @@
 package com.korikmat.watchtogether.utils
 
 object Constants {
+    const val SPECIFY_GENRES_ROUTES: String = "specify_genres_route"
+    const val EDIT_USER_PROFILE_ROUTES: String = "edit_user_profile_route"
     const val PROFILE_ROUTES: String = "profile_route"
     const val MOVIES_ROUTES: String = "movies_route"
     const val MATCHES_ROUTES: String = "matches_route"
+    const val MOVIE_DETAILS_ROUTES: String = "movie_details_route"
 }
